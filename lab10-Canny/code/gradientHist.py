194086a908568f1b40b6c6ac9263682831679638
MadCreeper <sunjichen21@126.com>
2021-11-24 20:48:52 +0800

    done lab10 coding

--- lab10-Canny/code/gradientHist.py
+++ lab10-Canny/code/gradientHist.py
@@ -3,6 +3,7 @@ from cv2 import cv2 # make vscode not complain
 from matplotlib import pyplot as plt
 import math
 import numpy as np
+import copy
 def combine_grad(sx, sy):
     if sx.shape != sy.shape:
         raise ValueError 
@@ -11,10 +12,13 @@ def combine_grad(sx, sy):
     print(grad_M.shape)
     for i in range(HEIGHT):
         for j in range(WIDTH):
-            grad_M[i][j] = np.sqrt(np.square(sx[i][j]) + np.square(sy[i][j]) )
+            grad_M[i][j] = np.sqrt( sx[i][j] *  sx[i][j] + sy[i][j] * sy[i][j] )
+    
+    grad_M = grad_M * 255 / np.max(grad_M)
     return grad_M
 
-def nonMaxSuppression(M,dx,dy):
+def nonMaxSuppression(img,dx,dy):
+    M = copy.deepcopy(img)
     if not (M.shape == dx.shape and M.shape == dy.shape):
         raise ValueError 
     HEIGHT, WIDTH = M.shape[0], M.shape[1]
@@ -24,9 +28,9 @@ def nonMaxSuppression(M,dx,dy):
             gradx = float(dx[i][j])
             grady = float(dy[i][j])
             if np.abs(gradx) < EPS:
-                gradx = np.sign(gradx) * EPS
+                gradx = EPS
             if np.abs(grady) < EPS:
-                grady = np.sign(grady) * EPS
+                grady = EPS
                 
             #gradm = M[i][j]
             if np.abs(grady) > np.abs(gradx):  # 梯度绝对值y方向大于x方向
@@ -56,32 +60,54 @@ def nonMaxSuppression(M,dx,dy):
             gtmp2 = w * g3 + (1 - w) * g4
             if M[i][j] < gtmp1 or M[i][j] < gtmp2:
                 M[i][j] = 0    
-                    
+    return M
+
+def doubleThresh(M,low_th,hi_th):
+    img = copy.deepcopy(M)
+    HEIGHT, WIDTH = img.shape[0], img.shape[1]
+    for i in range(1, HEIGHT - 1):
+        for j in range(1, WIDTH - 1):
+            if img[i][j] < low_th:
+                img[i][j] = 0
+            elif img[i][j] > hi_th:
+                img[i][j] = 255
+            elif (img[i-1][j-1:j+1] < hi_th).any() or (img[i+1][j-1:j+1] < hi_th).any() or (img[i][j-1] < hi_th) or (img[i][j+1] < hi_th): # 8邻域中有小于hi_th的点
+                img[i][j] = 255
+    return img
+
 def docanny(filename):
-    SAVEDIR = 'plot/'
+    SAVEDIR = 'result/'
     READDIR = 'dataset/'
-    img_gray = cv2.imread(READDIR + filename,cv2.IMREAD_GRAYSCALE)
+    lo_th, hi_th = 60, 150
+
+    img_gray = cv2.imread(READDIR + filename, cv2.IMREAD_GRAYSCALE)
     img_gauss= cv2.GaussianBlur(img_gray,(3,3),0)
-    sx = cv2.convertScaleAbs(cv2.Sobel(img_gauss,cv2.CV_16S,1,0))  # x方向梯度
-    sy = cv2.convertScaleAbs(cv2.Sobel(img_gauss,cv2.CV_16S,0,1))  # y方向梯度
-    M = cv2.convertScaleAbs(combine_grad(sx.astype(int),sy.astype(int)))
+    sx = cv2.convertScaleAbs(cv2.Sobel(img_gauss, cv2.CV_16S, 1, 0))  # x方向梯度
+    sy = cv2.convertScaleAbs(cv2.Sobel(img_gauss, cv2.CV_16S, 0, 1))  # y方向梯度
+    M = cv2.convertScaleAbs(combine_grad(sx.astype(int), sy.astype(int)))
     #print(M)
-    nonMaxSuppression(M,sx,sy)
-    cv2.imshow(filename,M)
+    img_sup = nonMaxSuppression(M, sx, sy)
+    
+    cv2.imshow(filename, M)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
+    img_fin = doubleThresh(img_sup, lo_th, hi_th)
+
+    cv2.imwrite(f"{SAVEDIR}/lowTH{lo_th}_hiTH{hi_th}_{filename}", img_fin)
     """
-    img_grad = calcGradient(img_gray)
-    bins = np.array(range(int(np.max(img_grad))))  - 0.01  # 防止值落在边界上
-    plt.hist(img_grad.ravel(),bins=bins,density=True,rwidth=1)
-    
-    plt.title('Gradient Histogtam of ' + filename)
-    plt.savefig(SAVEDIR + filename +'_GRAD_Hist.png')
-    #plt.show()
-    plt.close()
+    cv2.imshow(filename, img_fin)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     """
-    
+
+def docanny_builtin(filename):
+    SAVEDIR = 'result/'
+    READDIR = 'dataset/'
+    lo_th, hi_th = 60, 150
+    img_gray = cv2.imread(READDIR + filename, cv2.IMREAD_GRAYSCALE)
+    img_fin = cv2.Canny(img_gray, lo_th, hi_th)
+    cv2.imwrite(f"{SAVEDIR}/lowTH{lo_th}_hiTH{hi_th}_builtin_{filename}", img_fin)
 
 if __name__ == '__main__':
     files = [
@@ -91,6 +117,7 @@ if __name__ == '__main__':
     ] 
     for file in files:
         docanny(file)  
+        docanny_builtin(file)
 
 
 
